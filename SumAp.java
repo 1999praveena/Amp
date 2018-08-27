282d42054d40a061149387796894baa750a1a0a2
Praveena A M <praveenaarokiaraj@gmail.com>
2018-08-27 13:53:25 +0530

    Create SumAp.java

---
+++ SumAp.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+
+ class SumAp
+ {
+    public static void main(String[] arg)
+    {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter first term, common difference and n (number of terms)");
+    int a=sc.nextInt();
+    d=sc.nextInt();
+    n=sc.nextInt();
+    System.out.print("Sum: "+n*(2*a+(n-1)*d)/2);
+    }
+}
